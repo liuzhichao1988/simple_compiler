59448f83ac80bd929bd7bb2c08a54a34b42dfe33
neil <kenga@126.com>
2018-05-09 12:52:56 +0800

    Initial Commit

---
+++ simple_compiler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simple_compiler
+//
+//  Created by 刘志超 on 2018/5/9.
+//  Copyright © 2018年 刘志超. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
